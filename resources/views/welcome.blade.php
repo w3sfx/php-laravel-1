45e6fa25a8e1eb11e623d87c5713c8619e322907
w3sfx <w3sleyv1ctor@gmail.com>
2021-09-06 18:13:57 -0300

    praticas

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,38 +1,38 @@
-    @extends('layouts.main') {{--Utilizando layout da página especificada--}}
+@extends('layouts.main') {{--Utilizando layout da página especificada--}}
 
-    @section('title', 'HDC Events') {{--Título do site--}}
+@section('title', 'HDC Events') {{--Título do site--}}
 
-    @section('content') {{--Conteúdo do site--}}
-    
-    <div id="search-container" class="col-md-12">
-        <h1>Busque um evento</h1>
-        <form action="">
-            <input type="text" id="search" name="search" class="form-control" placeholder="Procurar...">
-        </form>
-    </div>
-    <div id="events-container" class="col-md-12">
-        <h2>Próximos Eventos</h2>
-        <p class="subtitle">Veja os eventos dos próximos dias</p>
-        <div id="cards-container" class="row">
-            @foreach ($events as $event)
-                <div class="card col-md-3">
-                    <img src="/img/events/{{$event->image}}" alt="{{ $event->title }}">
-                    <div class="card-body">
-                        <p class="card-date">{{date('d/m/Y', strtotime($event->date))}}</p>
-                        <h5 class="card-title">{{$event->title}}</h5>
-                        <p class="card-participants">X Participantes</p>
-                        <a href="/events/{{$event->id}}" class="btn btn-primary">Saber mais</a>
-                    </div>
+@section('content') {{--Conteúdo do site--}}
+
+<div id="search-container" class="col-md-12">
+    <h1>Busque um evento</h1>
+    <form action="">
+        <input type="text" id="search" name="search" class="form-control" placeholder="Procurar...">
+    </form>
+</div>
+<div id="events-container" class="col-md-12">
+    <h2>Próximos Eventos</h2>
+    <p class="subtitle">Veja os eventos dos próximos dias</p>
+    <div id="cards-container" class="row">
+        @foreach ($events as $event)
+            <div class="card col-md-3">
+                <img src="/img/events/{{$event->image}}" alt="{{ $event->title }}">
+                <div class="card-body">
+                    <p class="card-date">{{date('d/m/Y', strtotime($event->date))}}</p>
+                    <h5 class="card-title">{{$event->title}}</h5>
+                    <p class="card-participants">X Participantes</p>
+                    <a href="/events/{{$event->id}}" class="btn btn-primary">Saber mais</a>
                 </div>
-            @endforeach
-            @if(count($events) == 0)
-                <p>Não há eventos disponíveis</p>
-            @endif
-        </div>
+            </div>
+        @endforeach
+        @if(count($events) == 0)
+            <p>Não há eventos disponíveis</p>
+        @endif
     </div>
+</div>
 
-    @endsection {{--Informando o fim da sessão do conteúdo--}}
+@endsection {{--Informando o fim da sessão do conteúdo--}}
 
-    <!--Comentário com HTML é renderizado na inspeção-->
+<!--Comentário com HTML é renderizado na inspeção-->
 
-    {{--Comentário do Blade não é renderizado na inspeção--}}
+{{--Comentário do Blade não é renderizado na inspeção--}}
